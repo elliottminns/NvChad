60ad77445be3e467fcc17d72238940b8ff6f8044
Elliott Minns <elliott.minns@pm.me>
2023-02-12 23:05:58 -0600

    fix clangd and add deno

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -89,9 +89,11 @@ lspconfig.rust_analyzer.setup({
   }
 })
 
+lspconfig.denols.setup{}
+
 lspconfig.clangd.setup({
   filetypes = {"c", "cpp", "h", "hpp"},
-  root_dir = util.root_pattern("configre.ac", ".git", ".clangd")
+  root_dir = util.root_pattern("configure.ac", ".git", ".clangd")
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
